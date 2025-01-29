331d1942797672b1b19648c44703d03e2bf3a15d
Radi0Activ3-1 <drakepd100@gmail.com>
2025-01-28 19:28:10 -0600

    Add files via upload

---
+++ Bday and Adress.py
@@ -0,0 +1,11 @@
+# TODO: Define address information
+address = "3214 Oakdale Ave Johnsburg, IL 60051 "
+
+# TODO: Define birthday information
+birthday = "July 26th 2000"
+
+# TODO: Combine address and birthday into one sentence
+sentence = f"My address is {address} and my birthday is {birthday}."
+
+# TODO: Print the combined sentence
+print(sentence)
